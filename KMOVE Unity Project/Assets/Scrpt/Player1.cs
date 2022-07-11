11aba9d7a2704756639db6199645848b26cfc930
SiyunParkinGit <firetruck433@gmail.com>
2022-07-11 14:20:52 +0900

    캐릭터공격
    캐릭터 공격 모션 추가
    적 캐릭터 상태 만드는중

--- KMOVE Unity Project/Assets/Scrpt/Player1.cs
+++ KMOVE Unity Project/Assets/Scrpt/Player1.cs
@@ -15,24 +15,45 @@ public class Player1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        Move();
+        Attack();
+    }
+
+    private void Move()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
             this.transform.forward = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
             this.GetComponent<Animator>().SetBool("IsRun", true);
+
+            float h = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+            float v = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+            Vector3 postionVector = new Vector3(h, 0, v);
+            this.transform.position += postionVector;
+
+            Debug.Log(postionVector);
         }
         else
-        {   this.GetComponent<Animator>().SetBool("IsRun", false);   }
-
-        float h = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float v = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-
-        Vector3 postionVector = new Vector3(h, 0, v);
-        this.transform.position += postionVector;
-
-
-       
-           
-            
+        { this.GetComponent<Animator>().SetBool("IsRun", false); }
 
     }
+
+    private void Attack()
+    {
+      var but = Input.GetButtonDown("Fire1");
+        if (but == true)
+        {
+            GetComponent<Animator>().SetBool("IsRun", true);
+            GetComponent<Animator>().SetBool("IsAttack", true);
+
+        }
+    }
+
+    public void EndAttack()
+    {
+            GetComponent<Animator>().SetBool("IsAttack", false);
+    }
+
 }
+
